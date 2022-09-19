c5c19355d2f1260df21812432500e057ce33cfd3
Binh Pham <binhpham93bmt@gmail.com>
2022-09-20 02:03:06 +0700

    Lab02_01

---
+++ src/Lesson_02/Lab02_1.java
@@ -0,0 +1,29 @@
+package Lesson_02;
+
+import java.util.Scanner;
+
+public class Lab02_1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please Enter Height(m): ");
+        double Height = scanner.nextDouble();
+        System.out.print("Please Enter Weight(kg) : ");
+        double Weight = scanner.nextDouble();
+
+        double BMI =  Weight/(Height * 2);
+        System.out.println("BMI:  " + BMI);
+        System.out.println(" Chao ban, Chi so can nang cua ban dang o trang thai : ");
+
+        if (BMI <= 18.5){
+            System.out.printf("\t--->Ban hoi thieu can - Ban can bo sung dinh duong\n");
+        } else if (BMI >= 18.5 && BMI <= 24.9) {
+            System.out.printf("\t--->Chi so BMI dat tieu chuan - Co gang duy tri nhe ban  (✿◠‿◠)\n");
+        } else if (BMI >=25 && BMI <= 29.9) {
+            System.out.printf("\t--->Chi so BMI cua ban hoi cao 1 xiu roi nhe !!!\n");
+        } else
+            System.out.printf("\t--->BMI vuot nguong cho phep -Ban nen co che do an uong & tap duyen tot hon nhe\n");
+
+
+    }
+}
