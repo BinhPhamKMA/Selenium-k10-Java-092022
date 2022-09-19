7fd01784f62d1c85b67b93839bf24f6f1dd0042c
Binh Pham <binhpham93bmt@gmail.com>
2022-09-19 07:19:01 +0700

    vidu012

---
+++ src/VariableLearning.java
@@ -0,0 +1,9 @@
+public class VariableLearning {
+    public static void main(String[] args){
+        int myIntNum=18;
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+
+
+    }
+}
